8bce9e43817adc635a522dbfb5fadde6b297f5db
Rafif <Rafif@Rafif-PC>
2015-02-27 20:16:17 +0100

    My first test .

---
+++ project1/src/project1/Class1.java
@@ -0,0 +1,17 @@
+package project1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello word !");
+		
+		Class2 c1= new Class2();
+		c1.affiche();
+		
+		System.out.println("test 1");	
+		
+
+	}
+
+}
---
+++ project1/src/project1/Class2.java
@@ -0,0 +1,14 @@
+package project1;
+
+public class Class2 {
+
+	public void affiche (){
+		int s=0;
+		for(int i=0;i<=5;i++)
+			s=s+i;
+		
+		System.out.println("le resultat de la somme est :" +s);
+		
+		
+	}
+}
